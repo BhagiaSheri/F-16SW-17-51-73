82de49d2aff28f108a3ce5ebab73ca08b5da3c6b
BhagiaSheri <38161609+BhagiaSheri@users.noreply.github.com>
2019-01-26 12:27:02 +0500

    lab 9 subbmited

---
+++ lab 9/main/java/com/example/poojakumari/serviceslab/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.poojakumari.serviceslab;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "1";
+    Intent i;
+    Notification mNotification;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View view) {
+     i = new Intent(MainActivity.this,Myservice.class);
+     startService(i);
+    }
+
+    public void stopService(View view) {
+    stopService(i);
+
+    }
+
+}
